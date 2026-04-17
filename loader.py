8aa8adfb23996fcaa9bb76a3cc9d48ff9e47b75a
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-17 11:00:08 +0530

    fixed error in the file regarding the conditional statement for checking the file type and added a function to load data from csv and excel files.

--- loader.py
+++ loader.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 def load_data(file):
+    df=None
     if file is not None:
         file_name=file.name
-    if file_name.endswith(".csv"):
-        df=pd.read_csv(file)
-    elif file_name.endswith(".xlsx",".xls"):
-        df=pd.read_excel(file)
+        if file_name.endswith(".csv"):       
+            df=pd.read_csv(file)
+        elif file_name.endswith((".xlsx",".xls")):
+            df=pd.read_excel(file)
     return df
