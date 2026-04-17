41624fae490e5d035d73a7546a242fe926bbf1b3
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-17 10:29:54 +0530

    writing the function which will take data form the user and then load the data , so that we can do the further analysis.

---
+++ loader.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+def load_data(file):
+    if file is not None:
+        file_name=file.name
+    if file_name.endswith(".csv"):
+        df=pd.read_csv(file)
+    elif file_name.endswith(".xlsx",".xls"):
+        df=pd.read_excel(file)
+    return df
