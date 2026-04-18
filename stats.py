d18ffac68c6d4acb47c6b5fa5c68165fe27587b9
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-18 11:46:07 +0530

    fixing errors in stats.py.

--- stats.py
+++ stats.py
@@ -4,12 +4,14 @@ import numpy as np
 def clean_data(df):
     ok=st.button("Clean data")
     if ok:
-        df_cleaned=st.dataframe(df.dropna()) # Remove rows with missing values
-        df_cleaned=st.dataframe(df_cleaned.drop_duplicates()) # Remove duplicate rows
+        df_cleaned = df.dropna()  # Remove rows with missing values
+        df_cleaned = df_cleaned.drop_duplicates()  # Remove duplicate rows
+        st.dataframe(df_cleaned)  # Display the cleaned data
         return df_cleaned
-def basic_stats(df):
-    stats=st.dataframe(df.describe())  # Get basic statistics
-    return stats
+    def basic_stats(df):
+        stats= df.describe()  # Get basic statistics
+        st.dataframe(stats)  # Display it
+        return stats
 def dataset_info(df):
     st.write("Basic Information about the dataset:", df.info())  # Get dataset info
     st.write("Dataset Shape:", df.shape)  # Get dataset shape
