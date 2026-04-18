463e49ddfd462ee193e3183a82288a50aeb1fedf
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-18 11:40:02 +0530

    Making sure that the basic cleaning is done

--- stats.py
+++ stats.py
@@ -2,14 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 def clean_data(df):
-    df_cleaned=df.dropna()  # Remove rows with missing values
-    df_cleaned=df_cleaned.drop_duplicates()  # Remove duplicate rows
-    return df_cleaned
+    ok=st.button("Clean data")
+    if ok:
+        df_cleaned=st.dataframe(df.dropna()) # Remove rows with missing values
+        df_cleaned=st.dataframe(df_cleaned.drop_duplicates()) # Remove duplicate rows
+        return df_cleaned
 def basic_stats(df):
-    stats=df.describe()  # Get basic statistics
+    stats=st.dataframe(df.describe())  # Get basic statistics
     return stats
 def dataset_info(df):
-    print("Basic Information about the dataset:", df.info())  # Get dataset info
-    print("Dataset Shape:", df.shape)  # Get dataset shape
-    print("Missing Values in each column:\n", df.isnull().sum())  # Get missing values count
-    print("Correlation Matrix:\n", df.corr())  # Get correlation matrix
+    st.write("Basic Information about the dataset:", df.info())  # Get dataset info
+    st.write("Dataset Shape:", df.shape)  # Get dataset shape
+    st.write("Missing Values in each column:\n", df.isnull().sum())  # Get missing values count
+    st.write("Correlation Matrix:\n", df.corr())  # Get correlation matrix
