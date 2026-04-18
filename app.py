f0935059b081438c384a91de940c0f698149d181
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-18 11:42:03 +0530

    testing the function of stats.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from loader import load_data
+from stats import clean_data, basic_stats, dataset_info
 st.title("Data Viewer App")
 data=st.file_uploader("Upload your data",type=["csv","xlsx","xls"])
 df=load_data(data)
@@ -9,3 +10,4 @@ if df is not None:           # Only runs if df has data
     st.dataframe(df.head(10))
 else:
     st.write("Please upload a CSV or Excel file")  # Show message if nothing uploaded
+df_cleaned=clean_data(df)
