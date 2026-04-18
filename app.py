5468f8075ad15e9e68b784032a27841b89e41952
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-18 12:12:48 +0530

    add the visusalistaion page also

--- app.py
+++ app.py
@@ -5,16 +5,20 @@ from loader import load_data
 from stats import clean_data, basic_stats, dataset_info
 df_cleaned=None
 st.title("Data Viewer App")
-data=st.file_uploader("Upload your data",type=["csv","xlsx","xls"])
-df=load_data(data)
-if df is not None:           # Only runs if df has data
-    st.dataframe(df.head(10))
-else:
-    st.write("Please upload a CSV or Excel file")  # Show message if nothing uploaded
-if df is not None:
-    st.write("Dataset Information:")
-    dataset_info(df)
-    df_cleaned=clean_data(df)
-if df_cleaned is not None:
-    st.write("Basic Statistics:")
-    basic_stats(df_cleaned)
+
+
+page=st.sidebar.selectbox("Select a page", ["Basic Analysis","Charts and Visualizations"])
+if page=="Basic Analysis":
+    data=st.file_uploader("Upload your data",type=["csv","xlsx","xls"])
+    df=load_data(data)
+    if df is not None:           # Only runs if df has data
+        st.dataframe(df.head(10))
+    else:
+        st.write("Please upload a CSV or Excel file")  # Show message if nothing uploaded
+    if df is not None:
+        st.write("Dataset Information:")
+        dataset_info(df)
+        df_cleaned=clean_data(df)
+    if df_cleaned is not None:
+        st.write("Basic Statistics:")
+        basic_stats(df_cleaned)
