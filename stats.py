b9a94bd611016730bca87c558a80e1356f106170
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-19 23:29:50 +0530

    update stats.py

--- stats.py
+++ stats.py
@@ -1,20 +1,22 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 import numpy as np
+
+
 def clean_data(df):
-    ok=st.button("Clean data")
-    if ok:
-        df_cleaned = df.dropna()  # Remove rows with missing values
-        df_cleaned = df_cleaned.drop_duplicates()  # Remove duplicate rows
-        st.dataframe(df_cleaned)  # Display the cleaned data
-        return df_cleaned
+    df_cleaned = df.dropna()
+    df_cleaned = df_cleaned.drop_duplicates()
+    return df_cleaned
+
+
 def basic_stats(df):
-    stats= df.describe()  # Get basic statistics
-    st.dataframe(stats)  # Display it
-    st.write("Correlation Matrix:\n", df.select_dtypes(include=[np.number]).corr())  # Get correlation matrix
+    stats = df.describe()
+    st.dataframe(stats)
+    st.write("Correlation Matrix:\n", df.select_dtypes(include=[np.number]).corr())
     return stats
+
+
 def dataset_info(df):
-    st.write("Basic Information about the dataset:", df.info())  # Get dataset info
-    st.write("Dataset Shape:", df.shape)  # Get dataset shape
-    st.write("Missing Values in each column:\n", df.isnull().sum())  # Get missing values count
-    
+    st.write("Basic Information about the dataset:", df.info())
+    st.write("Dataset Shape:", df.shape)
+    st.write("Missing Values in each column:\n", df.isnull().sum())
