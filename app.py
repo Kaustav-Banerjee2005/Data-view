acae45c299c94072236b2e021bed1732e0527f45
Kaustav-Banerjee2005 <kaustabbanerjee188@gmail.com>
2026-04-19 21:01:26 +0530

    called the plots.py file, which contains the code for generating different types of charts based on user selection. The changes made in the app.py file are related to the integration of the plotting functionality into the main application. The commit message should reflect these changes and provide a clear description of what was modified and why.

--- app.py
+++ app.py
@@ -3,22 +3,39 @@ import pandas as pd
 import numpy as np
 from loader import load_data
 from stats import clean_data, basic_stats, dataset_info
-df_cleaned=None
+
 st.title("Data Viewer App")
 
+# Initialize session state variables
+if 'df' not in st.session_state:
+    st.session_state.df = None
+if 'df_cleaned' not in st.session_state:
+    st.session_state.df_cleaned = None
 
 page=st.sidebar.selectbox("Select a page", ["Basic Analysis","Charts and Visualizations"])
+
+# File uploader in sidebar (available on all pages)
+data=st.sidebar.file_uploader("Upload your data",type=["csv","xlsx","xls"])
+if data is not None:
+    st.session_state.df = load_data(data)
+
 if page=="Basic Analysis":
-    data=st.file_uploader("Upload your data",type=["csv","xlsx","xls"])
-    df=load_data(data)
-    if df is not None:           # Only runs if df has data
-        st.dataframe(df.head(10))
-    else:
-        st.write("Please upload a CSV or Excel file")  # Show message if nothing uploaded
-    if df is not None:
+    if st.session_state.df is not None:
+        st.dataframe(st.session_state.df.head(10))
+    
+    
+    if st.session_state.df is not None:
         st.write("Dataset Information:")
-        dataset_info(df)
-        df_cleaned=clean_data(df)
-    if df_cleaned is not None:
+        dataset_info(st.session_state.df)
+        st.session_state.df_cleaned = clean_data(st.session_state.df)
+    
+    if st.session_state.df_cleaned is not None:
         st.write("Basic Statistics:")
-        basic_stats(df_cleaned)
+        basic_stats(st.session_state.df_cleaned)
+
+elif page=="Charts and Visualizations":
+    if st.session_state.df is not None:
+        from plots import create_visualization
+        create_visualization(st.session_state.df)
+    else:
+        st.write("Please upload data first on the Basic Analysis page")
